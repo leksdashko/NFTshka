04771f83c44157f91e0656b1a698e46e6c1eb2d2
Oleksandr Dashko <leksdashko@gmail.com>
2022-09-22 20:38:13 +0200

    nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
+
   <ThemeProvider attribute="class">
     <div className="dark:bg-nft-dark bg-white min-h-screen">
       <Navbar />
