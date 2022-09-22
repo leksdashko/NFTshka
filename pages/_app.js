1311acc67ac3913dae7eaec28326c1a1bfac24ce
Oleksandr Dashko <leksdashko@gmail.com>
2022-09-22 12:48:15 +0200

    reusable components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+import { Footer, Navbar } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
 
-export default MyApp
+    <Script src="https://kit.fontawesome.com/7c6e3b59a4.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
